c59b112e9c38682cde934664402a4bb7d2cd30b0
Volodymyr Todosiuk <v.todosiuk@gmail.com>
2017-01-09 16:15:12 +0200

    Commit 09.01.2017/2

---
+++ src/test/java/dispatcher/controller/test/ReportControllerTest.java
@@ -0,0 +1,37 @@
+package dispatcher.controller.test;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.view;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.InjectMocks;
+import org.mockito.MockitoAnnotations;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import dispatcher.controller.ReportController;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(locations = { "classpath:applicationContextTest.xml", "springContextTest.xml" })
+public class ReportControllerTest {
+	@InjectMocks
+	private ReportController reportController;
+
+	private MockMvc mockMvc;
+
+	@Before
+	public void setup() {
+		MockitoAnnotations.initMocks(this);
+		mockMvc = MockMvcBuilders.standaloneSetup(reportController).build();
+	}
+
+	@Test
+	public void testReportController() throws Exception {
+
+		mockMvc.perform(get("/reportController/pdf")).andExpect(status().isOk()).andExpect(view().name("pdfView"));
+	}
+}
--- src/test/java/dispatcher/dao/test/AllTests.java
+++ src/test/java/dispatcher/dao/test/AllTests.java
@@ -3,13 +3,17 @@ package dispatcher.dao.test;
 import org.junit.runner.RunWith;
 import org.junit.runners.Suite;
 import org.junit.runners.Suite.SuiteClasses;
+
+import dispatcher.controller.test.ExportToExcelControllerTest;
 import dispatcher.controller.test.LoginControllerTest;
 import dispatcher.controller.test.ProviderControllerTest;
+import dispatcher.controller.test.ReportControllerTest;
 import dispatcher.controller.test.SupplyControllerTest;
 
 @RunWith(Suite.class)
 @SuiteClasses({ LoginControllerTest.class, ProviderControllerTest.class, SupplyControllerTest.class,
-		ProviderDaoImplTest.class, SupplyDaoImplTest.class })
+		ExportToExcelControllerTest.class, ReportControllerTest.class, ProviderDaoImplTest.class,
+		SupplyDaoImplTest.class })
 public class AllTests {
 
 }
