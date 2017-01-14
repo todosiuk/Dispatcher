9552b5d42505bb00ecaf085744d64698fa45c047
Volodymyr Todosiuk <v.todosiuk@gmail.com>
2017-01-14 15:27:25 +0200

    Commit 14.01.2017

--- src/test/java/dispatcher/controller/test/ReportControllerTest.java
+++ src/test/java/dispatcher/controller/test/ReportControllerTest.java
@@ -3,7 +3,6 @@ package dispatcher.controller.test;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.view;
-
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
